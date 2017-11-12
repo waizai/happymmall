c573cb63a9c12c0ae8569f78d51e0183f6c077eb
dangxiaoli <dangxiaoli@meituan.com>
2017-11-12 18:29:57 +0800

    add

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,8 +2,8 @@
 * @Author: dangxiaoli
 * @Date:   2017-11-11 20:36:09
 * @Last Modified by:   dangxiaoli
-* @Last Modified time: 2017-11-12 17:30:54
+* @Last Modified time: 2017-11-12 18:00:15
 */
 'use strict';
-
+require('./index.css')
 require('../moudle.js')
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,12 @@
 * @Author: dangxiaoli
 * @Date:   2017-11-11 21:16:12
 * @Last Modified by:   dangxiaoli
-* @Last Modified time: 2017-11-12 17:36:37
+* @Last Modified time: 2017-11-12 18:21:39
 */
 const path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 
 var config = {
      entry: {
@@ -20,11 +22,21 @@ var config = {
      externals : {
      	jquery : 'window.jQuery'
      },
+    module: {
+        loaders: [
+          // 编译css并自动添加css前缀
+            { 
+                test: /\.css$/, 
+                loader: ExtractTextPlugin.extract("style-loader","css-loader")
+            }
+        ]
+    },
      plugins : [
         new webpack.optimize.CommonsChunkPlugin({
             name : 'common',
             filename : 'js/base.js'
-        })
+        }),
+        new ExtractTextPlugin("css/[name].css"),
      ]
 };
 
