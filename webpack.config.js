5f7d2df946af725482d1bbd2883ab99b194c6d11
dangxiaoli <dangxiaoli@meituan.com>
2017-11-16 23:45:42 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,17 @@
 * @Author: dangxiaoli
 * @Date:   2017-11-11 21:16:12
 * @Last Modified by:   dangxiaoli
-* @Last Modified time: 2017-11-15 00:36:25
+* @Last Modified time: 2017-11-16 23:33:48
 */
 const path = require('path');
 var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+//环境变量的配置，dev/online
+var WEBPACK_ENV = process.env.WEBPACK_ENV || 'dev';
+console.log(WEBPACK_ENV)
+
 
 //获取html-webpack-plugin参数的方法
 var getHtmlConfig = function (name) {
@@ -23,12 +28,13 @@ var getHtmlConfig = function (name) {
 
 var config = {
      entry: {
-        'common' : ['./src/pages/common/index.js','webpack-dev-server/client?http://localhost:8088/'],
+        'common' : ['./src/pages/common/index.js'],
         'index'  : ['./src/pages/index/index.js'],
         'login'  : ['./src/pages/login/index.js']
      },
      output: {
          path     : path.resolve(__dirname, './dist'),
+         publicPath : '/dist',
          filename : 'js/[name].js'
      },
      externals : {
@@ -55,5 +61,9 @@ var config = {
      ]
 };
 
+
+if(WEBPACK_ENV === 'dev'){
+    config.entry.common.push('webpack-dev-server/client?http://localhost:8088/');
+}
 
 module.exports = config;
